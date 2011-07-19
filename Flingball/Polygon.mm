65e0b2e51ea0b5c1d4849503e49a67f6a5ee1565
Nick Payne <nick@kurai.co.uk>
2011-07-19 08:35:06 +0100

    - position vector now set on Polygons based on bodyDef position (refs #48)

--- Flingball/Polygon.mm
+++ Flingball/Polygon.mm
@@ -25,6 +25,11 @@
 
 -(void) createForWorld: (b2World*)world {
     NSLog(@"adding polygon to world at %.2f, %.2f", bodyDef.position.x, bodyDef.position.y);
+    
+    // we assume that we've only set our bodyDef's position, and strictly speaking
+    // our position gets updated the minute we start to tick() but make sure it
+    // starts off correctly too
+    position.Set(bodyDef.position.x * PTM_RATIO, bodyDef.position.y * PTM_RATIO);
     bodyDef.userData = self;    
     body = world->CreateBody(&bodyDef);    
     fixtureDef.shape = &shapeDef;
